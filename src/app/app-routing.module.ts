f5cb62332fe172bfdf908d0521cd70c7012bca46
soulhave <soulhave@yahoo.com.br>
2022-10-19 23:38:47 -0300

    Adjusts routers and css

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,10 @@ import { ExpenseListComponent } from './components/expenses/expense-list/expense
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [{
-  path: "", component: ExpenseListComponent
-}];
+const routes: Routes = [
+  { path: "", redirectTo: "expense/list", pathMatch: "full" },
+  { path: "expense/list", component: ExpenseListComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
